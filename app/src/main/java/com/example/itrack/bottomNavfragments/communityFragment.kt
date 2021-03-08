dd60c42ff03bf232705f3c17a61d59b4ccf8e015
Joshie <kalilinux19701375@gmail.com>
2021-03-08 16:17:06 +0800

    community fragment

--- app/src/main/java/com/example/itrack/bottomNavfragments/communityFragment.kt
+++ app/src/main/java/com/example/itrack/bottomNavfragments/communityFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.widget.ViewFlipper
 import com.example.itrack.R
 
 class communityFragment : Fragment(R.layout.fragment_community) {
