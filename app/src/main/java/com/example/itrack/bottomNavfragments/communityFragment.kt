349ce53d07359a0cd8c3ed8b5e265a4b406f1c94
Joshie <kalilinux19701375@gmail.com>
2021-03-12 18:05:06 +0800

    community update

--- app/src/main/java/com/example/itrack/bottomNavfragments/communityFragment.kt
+++ app/src/main/java/com/example/itrack/bottomNavfragments/communityFragment.kt
@@ -1,5 +1,6 @@
 package com.example.itrack.bottomNavfragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,7 +9,25 @@ import android.view.ViewGroup
 import android.webkit.WebView
 import android.widget.ViewFlipper
 import com.example.itrack.R
+import android.net.Uri
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class communityFragment : Fragment(R.layout.fragment_community) {
 
-}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+          view.findViewById<ImageButton>(R.id.blogs).setOnClickListener{val intent  = Intent(Intent.ACTION_VIEW, Uri.parse("https://nuawoman.com/blog/6-tips-for-good-menstrual-hygiene/"))
+
+              getActivity()?.startActivity(intent)}
+
+           /*val blog = Intent(Intent.ACTION_VIEW, Uri.parse("https://nuawoman.com/blog/6-tips-for-good-menstrual-hygiene/"))
+            startActivity(blog)*/
+        }
+    }
+
+/*
+val intent = Intent (getActivity(), Main::class.java)
+getActivity().startActivity(intent)
+*/
